b38dcdf611fd9be1c8529831974b64253d7ccac5
hoedhub <40145591+hoedhub@users.noreply.github.com>
2023-01-28 11:56:05 +0700

    installad capacitor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "./app.css";
 import App from "./App.svelte";
 import { setupIonicSvelte } from "$ionic/svelte";
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+// Call the element loader after the app has been rendered the first time
+defineCustomElements(window);
 
 setupIonicSvelte();
 
